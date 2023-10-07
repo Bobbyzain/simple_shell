936d17a02209cabe3212717f025bc32f5b052d16
wittyj <wittyjessy2002@yahoo.com>
2023-10-07 15:21:10 -0700

    opening the main.h file

---
+++ main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* HEADER FILES */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+/* PROTOTYPES */
+
+/* STRUCTURES */
+
+#endif
